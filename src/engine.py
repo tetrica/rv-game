27de84c19935c08fa65fb6a1ef18e05165f0e94a
fluiz <fluiz.cba@gmail.com>
2020-02-28 22:14:09 -0400

    fase 1

---
+++ src/engine.py
@@ -0,0 +1,67 @@
+import tcod as libtcod
+
+from entity import Entity
+from input_handlers import handle_keys
+from map_objects.game_map import GameMap
+from render_functions import clear_all, render_all
+
+def main():
+    screen_width = 80
+    screen_height = 50
+
+    map_width = 80
+    map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
+    }
+
+    player = Entity(screen_width // 2, screen_height // 2, '@', libtcod.white)
+    npc = Entity(screen_width // 2 - 5, screen_height // 2, '@', libtcod.yellow)
+
+    entities = [npc, player]
+
+    libtcod.console_set_custom_font('img01.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+
+    libtcod.console_init_root(screen_width, screen_height, 'trabalho de RV', False)
+
+    con = libtcod.console_new(screen_width, screen_height)
+
+    game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        libtcod.console_flush()
+
+        clear_all(con, entities)
+
+        action = handle_keys(key)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
+
+        if exit:
+            return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+if __name__ == "__main__":
+    main()
---
+++ src/map_objects/tile.py
@@ -0,0 +1,9 @@
+class Tile:
+    """
+    A tile on a map. It may or may not be blocked, and may or may not block sight.
+    """
+    def __init__(self, blocked, block_sight=None):
+        self.blocked = blocked
+        
+        # By default, if a tile is blocked, it also blocks sight
+        self.block_sight = blocked if block_sight is None else block_sight
